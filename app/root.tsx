98c10c7c57d03ebc50aa6bec80b84a1eafb2c2bc
Vladimir <vladgray777@gmail.com>
2025-11-20 22:08:58 -0500

    Your descriptive commit message here

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
-
+// Vladimir
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
